602f320dd3690e9761e09de6d751fc77048599ad
Alejo <alejosalles@gmail.com>
2014-04-14 20:34:49 -0300

    modified analyisis, in process

--- banditAnalysis.py
+++ banditAnalysis.py
@@ -2,6 +2,7 @@
 
 #import BanditExperiment
 #import BanditGame
+import utils
 import ExperimentData
 import matplotlib.pyplot as plt
 
@@ -37,6 +38,27 @@ def type2_perfo_plot():
     plt.show()
 
 
+def type2_perfo_plot_binned():
+
+    # pairs1=[(dif1,rep1) for dif1,rep1 in type2_perfo([1])]
+    dif1,rep1=type2_perfo([1])
+    dif2,rep2=type2_perfo([2])
+    jitrep2=[0.96*rep+0.02 for rep in rep2]
+
+    bave,bcenters,num=utils.binned_average(zip(dif1,rep1), nbins=5)
+    print bave, bcenters
+    plt.bar(bcenters, bave)
+    plt.show()
+
+    # plt.plot(dif1, rep1, 'bo')
+    # plt.plot(dif2, jitrep2, 'rs')
+    # plt.xlim([-0.7, 0.7])
+    # plt.ylim([-0.02, 1.02])
+    # plt.legend(['1', '2'])
+    # plt.show()
+
+
+
 def confidence_plot():
     payrates=data.get_payrates(metacog_types=[2])
     reports=data.get_reports(metacog_types=[2],report_part=1)
@@ -53,7 +75,8 @@ def confidence_plot():
 
 def main():
     #type2_perfo_plot()
-    confidence_plot()
+    #confidence_plot()
+    type2_perfo_plot_binned()
 
 if __name__ == '__main__':
     main()
